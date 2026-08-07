0771fdbbbe35c33d540e36ff702cc9aade6224f9
Ananta Sinha <97803273+ananta-mimo@users.noreply.github.com>
2026-08-07 16:48:53 -0400

    top and bottom lable changed

--- app.py
+++ app.py
@@ -574,20 +574,20 @@ with col_e:
     ax4.grid(axis="x", color="#E8E4DE", linewidth=0.6)
 
     if is_eb:
-        bottom_label = "◀ Near Beltway (I-495)"
-        top_label    = "◀ Near Washington DC"
+        bottom_label = "▼ Near Beltway (I-495)"
+        top_label    = "▲ Near Washington DC"
     else:
-        bottom_label = "◀ Near Washington DC"
-        top_label    = "◀ Near Beltway (I-495)"
+        bottom_label = "▼ Near Washington DC"
+        top_label    = "▲ Near Beltway (I-495)"
 
     ax4.annotate(bottom_label,
                 xy=(0, 0), xycoords=('axes fraction', 'axes fraction'),
-                xytext=(-0.5, -0.1), textcoords='axes fraction',
-                fontsize=9, color="#6A8AA0", fontstyle="italic")
+                xytext=(-0.4, -0.1), textcoords='axes fraction',
+                fontsize=9, color="#1769A0", fontstyle="italic")
     ax4.annotate(top_label,
                 xy=(0, 1), xycoords=('axes fraction', 'axes fraction'),
-                xytext=(-0.5, 1.02), textcoords='axes fraction',
-                fontsize=9, color="#6A8AA0", fontstyle="italic")
+                xytext=(-0.4, 1.02), textcoords='axes fraction',
+                fontsize=9, color="#1769A0", fontstyle="italic")
 
     plt.tight_layout()
 
